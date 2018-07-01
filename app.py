be7c073ce5c3b58ec24e3aa7036e2764208d6eac
Shay <shay@nutrinohealth.com>
2018-07-02 01:19:01 +0300

    added rank

--- app.py
+++ app.py
@@ -27,33 +27,41 @@ def check_time(time_str):
 def get_standings():
     res = requests.get('http://worldcup.sfg.io/matches')
     matches = filter(lambda x: x['stage_name'] == 'Round of 16' and x['status'] in ['in progress', 'completed'], res.json())
-    matches = map(lambda x: [x['home_team_events'], x['away_team_events'], x['home_team_country'] + ' vs ' + x['away_team_country']], matches)
+    matches = map(lambda x: [x['home_team_events'], x['away_team_events'], x['home_team_country'] + ' vs ' + x['away_team_country'], x['home_team']['goals'], x['away_team']['goals']], matches)
 
-    out_list = []
+    out_list = {}
     for team_event in matches:
-        score = [0, 0]
-        for xevent in team_event[0]:
-            if check_time(xevent['time']):
-                if 'goal-own' == xevent['type_of_event']:
-                    score[1] += 1
-                elif 'goal' in xevent['type_of_event']:
-                    score[0] += 1
-
-        for xevent in team_event[1]:
-            if check_time(xevent['time']):
-                if 'goal-own' == xevent['type_of_event']:
-                    score[0] += 1
-                elif 'goal' in xevent['type_of_event']:
-                    score[1] += 1
-
-        if score[0] == score[1]:
+        if team_event[3] == team_event[4]:
             str_score = 'X'
-        elif score[0] > score[1]:
+        elif team_event[3] > team_event[4]:
             str_score = '1'
-        elif score[0] > score[1]:
+        elif team_event[3] > team_event[4]:
             str_score = '0'
 
-        out_list.append({'name': team_event[2], 'result': str_score})
+        out_list.update({team_event[2]: str_score})
+        #score = [0, 0]
+        #for xevent in team_event[0]:
+        #    if check_time(xevent['time']):
+        #        if 'goal-own' == xevent['type_of_event']:
+        #            score[1] += 1
+        #        elif 'goal' in xevent['type_of_event']:
+        #            score[0] += 1
+
+        #for xevent in team_event[1]:
+        #    if check_time(xevent['time']):
+        #        if 'goal-own' == xevent['type_of_event']:
+        #            score[0] += 1
+        #        elif 'goal' in xevent['type_of_event']:
+        #            score[1] += 1
+
+        #if score[0] == score[1]:
+        #    str_score = 'X'
+        #elif score[0] > score[1]:
+        #    str_score = '1'
+        #elif score[0] > score[1]:
+        #    str_score = '0'
+
+        #out_list.append({'name': team_event[2], 'result': str_score})
 
     return out_list
 
@@ -62,13 +70,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def present_scores():
-    #stand_dict = get_standings()
+    stand_dict = get_standings()
 
     with open('./data/playoffs_data.json', 'r') as infile:
         bet_dict = json.load(infile)
 
     match_name = filter(lambda x: x != 'name', bet_dict[0].keys())
-    return render_template('wc_rank.html', result=bet_dict, match_name=match_name)
+    for item in bet_dict:
+        tmp_rank = 0
+        for mname in match_name:
+            if mname in stand_dict and stand_dict[mname] == str(item[mname]):
+                tmp_rank += 1
+
+        item['rank'] = tmp_rank
+
+    sorted_bet_dict = sorted(bet_dict, key=lambda x: x['rank'], reverse=True)
+
+    return render_template('wc_rank.html', result=sorted_bet_dict, match_name=match_name)
 
 @app.route('/end_point')
 def endp():
